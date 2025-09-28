398ce68bd28ea600ec45449c3d8243bfc231599a
utkarsh <utkarshsingh132003@gmail.com>
2025-09-28 22:38:45 +0530

    vite port exposed for docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, 
+    port: 4173, 
+  },
+  preview: {
+    host: true, 
+    port: 4173, 
+  },
   plugins: [react(), tailwindcss()],
 })
